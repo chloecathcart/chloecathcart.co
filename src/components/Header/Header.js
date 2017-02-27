62c2dd9577bfeda09bdc8b5804eec121010a3f28
Chloe Cathcart <chloecathcart@Chloes-MacBook-Pro.local>
2017-02-27 14:59:11 +0000

    Website Changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,12 +7,8 @@ function Header() {
   		<nav>
         <div class="container">
           <div class="nav-content">
-      	    <Link id="logo" to="/"><h1>Chloe Cathcart</h1></Link>
-      			<div class="icons">
-      				<a href="https://dribbble.com/chloecathcart"target="_blank"><i class="icon-dribbble"></i></a>
-      				<a href="https://www.instagram.com/chloecathcart/" target="_blank"><i class="icon-instagram"></i></a>
-      				<a href="/public/images/chloecathcartcv.pdf" target="_blank"><i class="icon-attachment"></i></a>
-      			</div>
+      	    <Link id="logo" to="/" class='hvr-hang'><h1>CHLOECATHCART.CO<span> | Product Designer</span></h1></Link>
+      			<div class="about"><h1>About</h1></div>
           </div>
         </div>
   	  </nav>
