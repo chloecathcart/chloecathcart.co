3a05681713dc58109f735a16acca9850fb8a4e65
Chloe Cathcart <chloecathcart@Chloes-MacBook-Pro.local>
2017-02-27 20:59:06 +0000

    removing stupid files and folders

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,9 @@ function Header() {
         <div class="container">
           <div class="nav-content">
       	    <Link id="logo" to="/" class='hvr-hang'><h1>CHLOECATHCART.CO<span> | Product Designer</span></h1></Link>
-      			<div class="about"><h1>About</h1></div>
+      			<div class="about">
+              <h1><Link to="/about">About</Link></h1>
+            </div>
           </div>
         </div>
   	  </nav>
